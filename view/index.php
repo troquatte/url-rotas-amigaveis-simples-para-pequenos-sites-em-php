ebcd4e1424f20a6b7de3206c1bd36ba153488bc1
Dener Troquatte <d.troquatte20@yahoo.com.br>
2018-02-09 15:00:07 -0200

    Update index.php

--- view/index.php
+++ view/index.php
@@ -1,8 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<title>Rotas e url Amigável Simples</title>
+	<meta content="cache" http-equiv="cache-control">
+	<meta content="pt-br" http-equiv="content-language">
+	<meta content="3 days" http-equiv="revisit-after">    
+	<meta content="index, follow" name="robots">
+	<meta name="description" content="Agencia de comunicacao e publicidade, aptos a colocar empresas ou linha de produtos em condicoes reais e em acao no mercado competitivo. Projetos estruturados, alinhando criatividade e objetivos de negocios. Trabalhando tambem com comunicacao empresarial."> 
+	<meta name='name' content='Roberta Macedo - Arquitetura - Engenharia - Decoração'>
+	<meta name='copyright' content='Filipe Mota'>
+	<meta content='MKT EW21' name='author'>
+	<meta name='url' content='http://www.filipemota.adv.br/'>
+	<meta name='category' content='Advocacia'>
+	<meta name="reply-to" content='mailto:adv@filipemota.adv.br'>
+	<meta content='Filipe Mota - Advocacia' property='og:title'>
+	<meta content='http://www.filipemota.adv.br/' property='og:url'>
+	<meta content='site' property='og:type'>
  
+ 	<meta property="og:image" content="http://www.filipemota.adv.br/view/images/post-facebook-site.png">
+	<meta property="og:image:type" content="image/jpeg">
+	<meta property="og:image:width" content="800"> /** PIXELS **/
+	<meta property="og:image:height" content="600"> /** PIXELS **/
 </head>
 <body>
 
@@ -20,3 +42,4 @@
 ?>
 
 </body> 
+</html>
